45968a7604837e0e38f7301f1bd2f1d73fc27094
Thilo Menke <tpmenke@icloud.com>
2024-07-19 22:57:46 +0200

    BiomSubdivRegionGenAlg

--- Godot/MapGenerator.gd
+++ Godot/MapGenerator.gd
@@ -12,7 +12,10 @@ func _ready():
 	VRNG.set_seed(rng.randi())
 	
 	var timestamp = Time.get_ticks_usec()
-	BBG([get_QC(Vector2i(0,0)).triangle1], [[0.01]], [100]) #[[0.08], [0]], [100, 3]
+	
+	BBG([get_QC(Vector2i(0,0)).triangle1], [[.0, 1], [.0, .3], [.1, 1]], [10, 18, 3], [false, true, false])
+	
+	#BBG([get_QC(Vector2i(0,0)).triangle1], [[0, 1], [0, .4], [0, .1], [.05, .8]], [45, 20, 25, 50], [true, true, true, false]) #[[0.08], [0]], [100, 3]
 	print(Time.get_ticks_usec()-timestamp)
 	print(QCs.size())
 	return
@@ -45,62 +48,66 @@ func get_QC(pPos:Vector2i):
 #region ** Biom Border Gen **
 
 func BBGDegration(pVal:int, args:Array) -> float:
+	if pVal == 0: return args[1]
 	return 2. ** (-args[0]*pVal)
 
-func BBG(pStartTriangle:Array,pChanceDegrationFunc:Array, pMaxDepth:Array):
+func BBG(pStartTriangle:Array, pChanceDegrationFunc:Array, pMaxDepth:Array, pLineExpansion:Array):
 	
 	var ccount:int = 1
 	
 	var visitedTriangles = {}
 	var frontierTriangles = pStartTriangle
 	
-	for s in len(pChanceDegrationFunc):
+	for t in frontierTriangles:
+		visitedTriangles[t] = true
 	
+	for s in len(pChanceDegrationFunc):
 		var tDegrationParams:Array = pChanceDegrationFunc[s]
 		var lastIterTriangles = []
+		var lineExpansion:bool =  pLineExpansion[s]
+		var temporaryDisabledTriangles = []
 			
 		for i in range(pMaxDepth[s]):
-			
-			print(str(i) + ": " + str(ccount))
-			
 			if len(frontierTriangles) == 0: break
 			
 			var chance:float = BBGDegration(i, tDegrationParams)
 			var newFrontierTriangles = []	
-			for t in range(0, len(frontierTriangles)):
-				var tTriangle:QCTriangle = frontierTriangles[t]
+			for tTriangle in frontierTriangles:
+				var aVec:Vector2i = Vector2i(1,1) if tTriangle.sideO else Vector2i(0,0)
+				var firstNeighborSelected:bool = false
+				var tTrianglePos = tTriangle.qc.pos 
 				
-				if VRNG.rand(tTriangle.qc.pos * 3 + (Vector2i(1,1) if tTriangle.sideO else Vector2i(0,0))) < chance:
+				if VRNG.rand(tTrianglePos * 3 + aVec) < chance:
 					var tNeighbors:Array = tTriangle.getNeighbors()
-					
-					for n in range(3):
-						var tneighbor = tNeighbors[n]
+					var tRange = range(3) if VRNG.bRand(tTrianglePos * 4 + aVec) else range(2, -1, -1)
+					var tA:int = 0
+					for n in tRange:
+						var tneighbor:QCTriangle = tNeighbors[n]
+						tA += 1
+						
 						if visitedTriangles.has(tneighbor): continue
+						
+						if firstNeighborSelected && lineExpansion:
+							if !visitedTriangles.has(tneighbor.qc.triangle1): temporaryDisabledTriangles.append(tneighbor.qc.triangle1)
+							if !visitedTriangles.has(tneighbor.qc.triangle2): temporaryDisabledTriangles.append(tneighbor.qc.triangle2)
+							visitedTriangles[tneighbor.qc.triangle1] = true
+							visitedTriangles[tneighbor.qc.triangle2] = true
+							continue
 						
 						newFrontierTriangles.append(tneighbor)
 						lastIterTriangles.append(tneighbor)
 						visitedTriangles[tneighbor] = true
 						ccount += 1
-						
-						var polyObj = PolygonObject.instantiate()
-						add_child(polyObj)
-						polyObj.polygon = PackedVector2Array(tneighbor.edges)
-						polyObj.material = DebugMaterial1 if i == 16 else DebugMaterial2
+						firstNeighborSelected = true
 			frontierTriangles = newFrontierTriangles
 		
+								
+		for tTriangle in temporaryDisabledTriangles:
+			visitedTriangles.erase(tTriangle)
+		
+		temporaryDisabledTriangles = []
+		
 		frontierTriangles = lastIterTriangles
-		
-		
-	#for t in visitedTriangles:
-	#	
-	#	var polyObj = PolygonObject.instantiate()
-	#	add_child(polyObj)
-	#	print(t.edges[0])
-	#	polyObj.polygon = PackedVector2Array(t.edges)
-	#	#polyObj.material = DebugMaterial1 if t.debugDiagonal else DebugMaterial2
-		
-		
-	print("BBG END " + str(ccount))
 	
 func BBGRecursive(pDepth:int, pMaxDepth:int, pChanceAtDepth:Array):
 	
