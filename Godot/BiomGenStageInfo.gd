de476687e46bd3336f96c50d976e6f178d65e8b8
Thilo Menke <tpmenke@icloud.com>
2024-07-21 20:20:19 +0200

    Terrain Progr

--- Godot/BiomGenStageInfo.gd
+++
@@ -1,7 +0,0 @@
-class_name BiomGenStateInfo extends Resource
-
-const PropertyCount:int = 3 # When increased: Update BiomInfo.SplitIndividualGenerationStages!
-
-@export var MaxDepth:int
-@export var IsLineGen:bool
-@export var ChanceDegrationArgs:Array
